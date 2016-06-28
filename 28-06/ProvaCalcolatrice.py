15823666174b7481966cb73e755423f571face8b
Douglas <douglas13mg@gmail.com>
2016-06-28 16:48:44 -0300

    add new test cases

--- 28-06/ProvaCalcolatrice.py
+++ 28-06/ProvaCalcolatrice.py
@@ -3,7 +3,12 @@ from Calcolatrice import *
 
 class ProvaCalcolatrice(unittest.TestCase):
 	def testSommaVettore(self):
-		vettore=[1,2,3]
+		vettore=[1, 2, 3]
 		self.assertEqual(sommaVettore(vettore),6)
-
+	def testSommaVettore2(self):
+		vettore=[1, 5.5, 2]
+		self.assertEqual(sommaVettore(vettore), 8.5)
+	def testSommaVettore3(self):
+		vettore=[5, 4]
+		self.assertEqual(sommaVettore(vettore), 9)
 unittest.main()
