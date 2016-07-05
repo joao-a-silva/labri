8460d817ebae40200e924afa45b06fc37c98c384
Diego Carvalho <diego.sistemas.carvalho@gmail.com>
2016-07-05 16:13:40 -0300

    sumMatrix improved

--- 28-06/Calcolatrice.py
+++ 28-06/Calcolatrice.py
@@ -6,3 +6,23 @@ def sommaVettore(vettore):
 		somma = somma + numero
 
 	return somma
+
+def sommaMatrice(matrice1, matrice2):
+	file1 = open(matrice1, "r")
+	file2 = open(matrice2, "r")
+
+
+	matriceRisultato=[]
+	
+	for linea in file1:
+		valores1=linea.replace("\n","").split(",")
+		valores2=file2.readline().replace("\n","").split(",")
+		vettoreSomma=[]
+		print valores2
+		print valores1	
+		for i in range(0, 3):
+			vettoreSomma.append(int(valores1[i])+int(valores2[i]))
+		matriceRisultato.append(vettoreSomma)
+	file1.close()
+	file2.close()
+	return matriceRisultato
