e8d0a9b4c03ee56f5794e0095491c34bb28d8509
Gustavo Franco <gustavodifrafre@gmail.com>
2016-07-12 15:30:20 -0300

    added error

--- 28-06/ProvaMatrice.py
+++ 28-06/ProvaMatrice.py
@@ -3,8 +3,8 @@ from Calcolatrice import *
 
 class ProvaMatrice(unittest.TestCase):
 	def testSommaMatrice(self):
-		matrice1="Matrice1"
-		matrice2="Matrice2"
+		matrice1="matrice1"
+		matrice2="matrice2"
 		self.assertEqual(sommaMatrice(matrice1, matrice2),[[0,0,0],[0,0,0],[0,0,0]])
 	
 unittest.main()
