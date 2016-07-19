2d3e89646206b57593c5ca8df2f85679fd4576f7
Gustavo Franco <gustavodifrafre@gmail.com>
2016-07-19 16:18:28 -0300

    class Calculatrice implemented

--- 19-07/HelloWorld.scala
+++ 19-07/HelloWorld.scala
@@ -1,14 +1,32 @@
+class Calculatrice(val x:Int, val y:Int){
+	def soma(): Int ={
+		//val result = x+y
+		return x + y
+	}
+
+	def if_method() : Int = {
+		val maximo = if (x > y) x else y
+		return maximo
+  	}
+
+
+
+}
+
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
-    println("Max value = " + if_method(15,21))
+
     for_method(Array(2,5,8,5,9,10,12))
+
+    var calc = new Calculatrice(5,3)
+    val result= calc.soma()
+    println("Result soma= " + result)
+        println("Max value = " + calc.if_method())
   }
 
-  def if_method(x:Int, y:Int ) : Int = {
-	val maximo = if (x > y) x else y
-	return maximo
-  }
+  
 
   def for_method(lista: Array[Int]): Unit = {
     //val lista = lista(1, 2, 3, 4)
@@ -19,4 +37,8 @@ object HelloWorld {
       }
     }
   }
+
+
+
+
 }
