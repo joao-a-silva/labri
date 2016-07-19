fb0ff28e370789f9d0c61c9ce9469e4c968fba14
Gustavo Franco <gustavodifrafre@gmail.com>
2016-07-19 15:42:56 -0300

    created first method with if clause

--- 19-07/HelloWorld.scala
+++ 19-07/HelloWorld.scala
@@ -1,5 +1,11 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
+    println("Max value = " + if_method(15,21))
+  }
+
+  def if_method(x:Int, y:Int ) : Int = {
+	val maximo = if (x > y) x else y
+	return maximo
   }
 }
